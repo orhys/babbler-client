d746c23ed1554bc671f1408aebc0d62298e36bc9
Orhys Brown <51386750+orhys@users.noreply.github.com>
2020-01-02 21:05:05 -0500

    completed top navbar w/ REACT Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup'
 
+import Navbar from "./components/Navbar";
 
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
-export class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-        <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/login" component={login} />
-          <Route exact path="/signup" component={signup} />
-
-        </Switch>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
         </Router>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
