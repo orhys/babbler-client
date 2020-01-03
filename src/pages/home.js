61b6ffeb30d8f5a3a536228a103bf8cb0f42f927
Orhys Brown <51386750+orhys@users.noreply.github.com>
2020-01-03 17:38:05 -0500

    working: on the look of the screams page

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,6 @@ export class home extends Component {
     axios
       .get("/screams")
       .then(res => {
-        console.log(res.data);
         this.setState({
           screams: res.data
         });
@@ -21,8 +20,8 @@ export class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <Scream scream={scream} />)
-    ) : (
+      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />
+    )) : (
       <p>Loading ...</p>
     );
     return (
