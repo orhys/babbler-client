9434490a5c7d56753e21ab40828a182398557904
Orhys Brown <51386750+orhys@users.noreply.github.com>
2020-01-03 21:16:17 -0500

    completed login page

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
-import { withStyles, Typography } from "@material-ui/core";
+import {
+  withStyles,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const styles = {
   card: {
     display: "flex",
-    marginBottom: 20,
+    marginBottom: 20
   },
   image: {
-      minWidth: 200,
+    minWidth: 200
   },
   content: {
-      padding: 25,
-      objectFit: 'cover',
+    padding: 25,
+    objectFit: "cover"
   }
 };
 
 export class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -35,10 +41,23 @@ export class Scream extends Component {
     } = this.props;
     return (
       <Card className={classes.card}>
-        <CardMedia image={userImage} title="Profile image" className={classes.image}/>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
         <CardContent className={classes.content}>
-          <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(createdAt).fromNow()}
+          </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 
-import Scream from '../components/Scream'
+import Scream from "../components/Scream";
 
 export class home extends Component {
   state = {
@@ -20,8 +20,10 @@ export class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />
-    )) : (
+      this.state.screams.map(scream => (
+        <Scream key={scream.screamId} scream={scream} />
+      ))
+    ) : (
       <p>Loading ...</p>
     );
     return (
