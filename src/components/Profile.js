a93610bfd5f07c9f92baf13d1e7585480ea55dfb
Orhys Brown <51386750+orhys@users.noreply.github.com>
2020-01-30 17:15:42 -0500

    working: profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
-import { withStyles, Button, Typography } from "@material-ui/core";
+import { withStyles, Button, Typography, Paper } from "@material-ui/core";
 import MuiLink from "@material-ui/core/Link";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 
 // Icons
 import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
-
 
 const styles = {
   paper: {
@@ -65,31 +65,81 @@ export class Profile extends Component {
       classes,
       user: {
         credentials: { handle, createdAt, imageUrl, bio, website, location },
-        loading
+        loading,
+        authenticated
       }
     } = this.props;
 
-    let profileMarkup = !loading ? (authenticated ? (
+    let profileMarkup = !loading ? (
+      authenticated ? (
         <Paper className={classes.paper}>
-            <div className={classes.profile}>
-                <div className="profile-image">
-                    <img src={imageUrl} alt="profile" />
-                </div>
-                <hr/>
-                <div className={profileDetails}>
-                <MuiLink component={Link} to ={`/users/${handle}`} color="primary" varient="h5">
-                    @{handle}
-                </MuiLink>
-                <hr/>
-                {bio && <Typography variant="body2">{bio}</Typography>}
-                <hr />
-                {location && (
-                    LocationOn color="primary />"
-                )}
-                </div>
+          <div className={classes.profile}>
+            <div className="profile-image">
+              <img src={imageUrl} alt="profile" />
             </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="primary"
+                varient="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LocationOn color="primary" /> <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              \
+              {website && (
+                <Fragment>
+                  <LinkIcon color="primary" />
+                  <a href={website} target="_blank" rel="noopener noreferrer">
+                    {" "}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarToday color="primary" />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </div>
+          </div>
         </Paper>
-    ) : ()) : (<p>loading...</p>)
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+      <p>loading...</p>
+    );
 
     return profileMarkup;
   }
@@ -99,7 +149,7 @@ const mapStateToProps = state => ({
   user: state.user
 });
 
-Profile.PropTypes = {
+Profile.propTypes = {
   user: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired
 };
